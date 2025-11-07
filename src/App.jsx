f5108ee1dfc557083685813cddfeed2706e1b90c
Panda-racoon <millie3519@gmail.com>
2025-11-08 00:32:11 +0900

    폴더구조만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./layout/AppLayout";
+
+// 페이지
+// 1. 홈페이지
+// 2. 영화 전체보여주는 페이지 (서치)
+// 3. 영화 디테일 페이지
 
 function App() {
-  return <>넷플릭스 만들거에여</>;
+  return (
+    <Routes>
+      {/* user화면 */}
+      <Route path="/" element={<AppLayout />} />
+    </Routes>
+  );
 }
 
 export default App;
